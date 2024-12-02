f53376b29ab8fc6cf62caf354f49330be745042e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-12-03 02:06:28 +0600

    Added the Hash File component

---
+++ src/Components/SpinnerComponent.jsx
@@ -0,0 +1,8 @@
+export default function SpinnerComponent() {
+    return (
+        <div className="spinner-container">
+            <div className="spinner"></div>
+            <p>Processing your file...</p>
+        </div>
+    );
+}
