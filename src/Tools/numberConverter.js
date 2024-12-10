6319fcb7afa38ad4b43f1634f13d6ae0f43482ee
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-12-11 04:36:15 +0600

    Formatted the code

--- src/Tools/numberConverter.js
+++ src/Tools/numberConverter.js
@@ -2,10 +2,7 @@ const toBinary = (n) => (n >> 0).toString(2);
 
 const toDecimal = (n) => parseInt(n, 2);
 
-const toOctal = (n) => {
-    n = +n;
-    return parseInt(n, 10).toString(8);
-}
+const toOctal = (n) => parseInt(n, 10).toString(8);
 
 const toHexadecimal = (n) => parseInt(n, 10).toString(16);
 
